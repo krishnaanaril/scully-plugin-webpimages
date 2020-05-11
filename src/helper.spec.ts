b98bc9590791a5768cc26335ac7bdd659dfd7560
Krishna <krishnamohan.a.m@gmail.com>
2020-05-11 19:33:16 +0530

    test: add test cases for helper.ts

---
+++ src/helper.spec.ts
@@ -0,0 +1,26 @@
+import { getMimetypeFromExtension } from './helper';
+
+describe('getMimetypeFromExtension method', () => {
+    test.each([
+        ['.jpg', 'image/jpeg'],
+        ['.jpeg', 'image/jpeg'],
+        ['.jfif', 'image/jpeg'],
+        ['.pjpeg', 'image/jpeg'],
+        ['.dummy', 'image/jpeg'],
+        ['', 'image/jpeg'],
+        ['.pjp', 'image/jpeg'],
+        ['.webp', 'image/webp'],
+        ['.tiff', 'image/tiff'],
+        ['.tif', 'image/tiff'],
+        ['.ico', 'image/x-icon'],
+        ['.cur', 'image/x-icon'],
+        ['.svg', 'image/svg+xml'],
+        ['.gif', 'image/gif'],
+        ['.png', 'image/png'],
+        ['.bmp', 'image/bmp'],
+        ['.apng', 'image/apng']        
+    ])('should evaluate %s as %s', (extension, expectedMimeType)=>{
+        const mimeType = getMimetypeFromExtension(extension);
+        expect(mimeType).toEqual(expectedMimeType);
+    });
+})
