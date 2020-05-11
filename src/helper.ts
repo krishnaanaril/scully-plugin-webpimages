64c5344c50595bc151fc4eeb0ca6e2b544a81800
Krishna <krishnamohan.a.m@gmail.com>
2020-05-11 20:01:32 +0530

    format: prettier changes

--- src/helper.ts
+++ src/helper.ts
@@ -1,40 +1,40 @@
 export const getMimetypeFromExtension = (extension: string): string => {
-    let mimeType;
-    switch (extension) {
-        case '.apng':
-            mimeType = 'image/apng';
-            break;
-        case '.bmp':
-            mimeType = 'image/bmp';
-            break;
-        case '.gif':
-            mimeType = 'image/gif';
-            break;
-        case '.ico':
-        case '.cur':
-            mimeType = 'image/x-icon';
-            break;
-        case '.png':
-            mimeType = 'image/png';
-            break;
-        case '.svg':
-            mimeType = 'image/svg+xml';
-            break;
-        case '.tiff':
-        case '.tif':
-            mimeType = 'image/tiff';
-            break;
-        case '.webp':
-            mimeType = 'image/webp';
-            break;
-        case '.jpg':
-        case '.jpeg':
-        case '.jfif':
-        case '.pjpeg':
-        case '.pjp':
-        default:
-            mimeType = 'image/jpeg';
-            break;
-    }
-    return mimeType;
-}
+  let mimeType;
+  switch (extension) {
+    case '.apng':
+      mimeType = 'image/apng';
+      break;
+    case '.bmp':
+      mimeType = 'image/bmp';
+      break;
+    case '.gif':
+      mimeType = 'image/gif';
+      break;
+    case '.ico':
+    case '.cur':
+      mimeType = 'image/x-icon';
+      break;
+    case '.png':
+      mimeType = 'image/png';
+      break;
+    case '.svg':
+      mimeType = 'image/svg+xml';
+      break;
+    case '.tiff':
+    case '.tif':
+      mimeType = 'image/tiff';
+      break;
+    case '.webp':
+      mimeType = 'image/webp';
+      break;
+    case '.jpg':
+    case '.jpeg':
+    case '.jfif':
+    case '.pjpeg':
+    case '.pjp':
+    default:
+      mimeType = 'image/jpeg';
+      break;
+  }
+  return mimeType;
+};
--- src/web-images.ts
+++ src/web-images.ts
@@ -5,45 +5,41 @@ import { JSDOM } from 'jsdom';
 import { getMimetypeFromExtension } from './helper';
 
 export const webpImagesPlugin = async (html: string, route: HandledRoute) => {
+  const dom = new JSDOM(html);
+  try {
+    const doc = dom.window.document;
+    const imageElements = doc.getElementsByTagName('img');
 
-    const dom = new JSDOM(html);
-    try {
-        const doc = dom.window.document;
-        const imageElements = doc.getElementsByTagName('img');
+    log('Number of images found: ' + imageElements.length);
 
-        log('Number of images found: ' + imageElements.length);
+    for (const element of Array.from(imageElements)) {
+      const imageRelativePath: any = element.getAttribute('src');
+      const imageAlternateText: any = element.getAttribute('alt');
+      const imageExtension = imageRelativePath.substr(imageRelativePath.lastIndexOf('.'));
+      const webpRelativePath = imageRelativePath.replace(imageExtension, '.webp');
 
-        for (const element of Array.from(imageElements)) {
-            const imageRelativePath: any = element.getAttribute('src');
-            const imageAlternateText: any = element.getAttribute('alt');
-            const imageExtension = imageRelativePath.substr(imageRelativePath.lastIndexOf('.'));
-            const webpRelativePath = imageRelativePath.replace(imageExtension, '.webp');
+      const pictureElement = doc.createElement('picture');
+      const sourceElementForWebp = doc.createElement('source');
+      sourceElementForWebp.setAttribute('type', 'image/webp');
+      sourceElementForWebp.setAttribute('srcset', webpRelativePath);
+      pictureElement.appendChild(sourceElementForWebp);
 
-            const pictureElement = doc.createElement('picture');
-            const sourceElementForWebp = doc.createElement('source');
-            sourceElementForWebp.setAttribute('type', 'image/webp');
-            sourceElementForWebp.setAttribute('srcset', webpRelativePath);
-            pictureElement.appendChild(sourceElementForWebp);
+      const sourceElementForUserImage = doc.createElement('source');
+      sourceElementForUserImage.setAttribute('type', getMimetypeFromExtension(imageExtension));
+      sourceElementForUserImage.setAttribute('srcset', imageRelativePath);
+      pictureElement.appendChild(sourceElementForUserImage);
 
-            const sourceElementForUserImage = doc.createElement('source');
-            sourceElementForUserImage.setAttribute('type', getMimetypeFromExtension(imageExtension));
-            sourceElementForUserImage.setAttribute('srcset', imageRelativePath);
-            pictureElement.appendChild(sourceElementForUserImage);
+      const newImageElement = doc.createElement('img');
+      newImageElement.setAttribute('src', imageRelativePath);
+      newImageElement.setAttribute('alt', imageAlternateText);
 
-            const newImageElement = doc.createElement('img');
-            newImageElement.setAttribute('src', imageRelativePath);
-            newImageElement.setAttribute('alt', imageAlternateText);
-
-            pictureElement.appendChild(newImageElement);            
-            element.parentNode?.replaceChild(pictureElement, element);
-        };
-
+      pictureElement.appendChild(newImageElement);
+      element.parentNode?.replaceChild(pictureElement, element);
     }
-    catch (error) {
-        logError(error);
-    }
-    finally {
-        log('Webp rendering complete.')
-    }
-    return dom.serialize();
-}
+  } catch (error) {
+    logError(error);
+  } finally {
+    log('Webp rendering complete.');
+  }
+  return dom.serialize();
+};
